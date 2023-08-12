7a8063daf01d965b9c19901e79f50b70c6291660
Jakob Ossmann <jakob.ossmann@griffithuni.edu.au>
2023-08-12 18:23:14 +1000

    week4 final

---
+++ html/webAppDev/week4/task5/assoc-laravel/resources/views/results.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.master')
+
+@section('title')
+  Australian Prime Ministers
+@endsection
+
+@section('content')
+
+    <h2>Australian Prime Ministers</h2>
+    <p>Your search results:</p>
+
+    <table class="bordered">
+    <!-- table header -->
+    <tr>
+        <th>No.</th>
+        <th>Name</th>
+        <th>From</th>
+        <th>To</th>
+        <th>Duration</th>
+        <th>Party</th>
+        <th>State</th>
+    </tr>
+    @forelse ($pms as $pm)
+        <tr>
+            <td>{{ $pm['index'] }}</td>
+            <td>{{ $pm['name'] }}</td>
+            <td>{{ $pm['from'] }}</td>
+            <td>{{ $pm['to'] }}</td>
+            <td>{{ $pm['duration'] }}</td>
+            <td>{{ $pm['party'] }}</td>
+            <td>{{ $pm['state'] }}</td>
+        </tr>
+    @empty
+        <tr>
+            <td colspan="7">No data available</td>
+        </tr>
+    @endforelse
+</table>
+
+@endsection
